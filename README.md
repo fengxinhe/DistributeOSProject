2613a0ad2894e1bd27d7e8a53999f110f046e4b1
Xinhe Feng <xf459@nyu.edu>
2018-04-08 17:31:46 -0400

    add description

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DistributeOSProject
-THe final project for the distributed os
+## The final project for the distributed os
+
+#### Member: only me, Xinhe Feng
+
+screenshots of basic web app:
+
+- index page, show all blogs, can do like and dislike
+ ![avatar](./screentshot/index.png)
+
+- login page
+![avatar](./screentshot/login1.png)
+
+- register page
+ ![avatar](./screentshot/register.png)
+
+- blog writing page
+ ![avatar](./screentshot/write.png)
--- src/controller/register.go
+++ src/controller/register.go
@@ -14,16 +14,23 @@ import (
 // RegisterGET displays the register page
 func RegisterGet(w http.ResponseWriter, r *http.Request) {
 	// Get session
-	sess := session.Instance(r)
+	//sess := session.Instance(r)
 
 	// Display the view
 	v := view.New(r)
 	v.Name = "register"
-	if sess.Values["authenticated"]==1{
-		v.Data["Username"] = sess.Values["username"]
-	}else{
-			v.Data["Username"] = "guest"
-	}
+	// if sess.Values["authenticated"]==1{
+	// 	v.Data["Username"] = sess.Values["username"]
+	// }else{
+	// 		v.Data["Username"] = "guest"
+	// }
+
+	userName := session.GetUserName(r)
+	if userName != "" {
+		v.Data["Username"]=userName
+	} else {
+		v.Data["Username"] = "guest"
+}
 	//sess := session.Instance(r)
 	// Refill any form fields
 //	view.Repopulate([]string{"first_name", "last_name", "email"}, r.Form, v.Data)
@@ -32,14 +39,8 @@ func RegisterGet(w http.ResponseWriter, r *http.Request) {
 
 // RegisterPOST handles the registration form submission
 func RegisterPost(w http.ResponseWriter, r *http.Request) {
-	// Get session
-//	sess := session.Instance(r)
 
-
-	// Validate with required fields
-
-	// Get form values
-	sess := session.Instance(r)
+	//sess := session.Instance(r)
 	userName := r.FormValue("user_name")
 	password := r.FormValue("user_password")
 
@@ -49,15 +50,15 @@ func RegisterPost(w http.ResponseWriter, r *http.Request) {
 	if err != nil { // If success (no user exists with that email)
 		model.UserCreate(userName, password)
 		// Will only error if there is a problem with the query
-		sess.Save(r,w)
+	//	sess.Save(r,w)
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	} else if err == nil { // Catch all other errors
-		sess.Save(r,w)
+	//	sess.Save(r,w)
 		log.Println(err)
 
 	} else { // Else the user already exists
-		sess.Save(r,w)
+	//	sess.Save(r,w)
 	}
 
 	// Display the page
